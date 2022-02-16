c86bbf7587f359b6941ec33e54f004fe77fb5d43
4Pavlo2 <pshkoropad@gmail.com>
2022-02-16 16:47:04 +0200

    add routes -questions \ create questions_controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   validates :title, presence: true, length: {minimum:2, maximum:40}
   validates :body, presence: true, length: {minimum:2, maximum:4000}
-end40
+end
